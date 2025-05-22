05e4fbd22d43d48a7ffaad89ae6e2edef9e94840
Ethan Fuller <etwan007@gmail.com>
2025-05-22 12:32:04 -0400

    Fancy Calendar Update

--- src/components/GoogleCalendarView.jsx
+++ src/components/GoogleCalendarView.jsx
@@ -4,9 +4,10 @@ import parse from 'date-fns/parse';
 import startOfWeek from 'date-fns/startOfWeek';
 import getDay from 'date-fns/getDay';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { enUS } from 'date-fns/locale';
 
 const locales = {
-  'en-US': require('date-fns/locale/en-US'),
+  'en-US': enUS,
 };
 
 const localizer = dateFnsLocalizer({
